5e5b82dc580291523d4f9a5c6c82cd059efeb618
Sergi Cladera <sergicladera@paucasesnovescifp.cat>
2021-12-13 17:32:49 +0100

    Creado el metodo en DataAcces que nos realiza la conexión con el servidor Azure donde se encuentra nuestra base de datos a partir de nuestro fichero properties(que no subiremos a github). Añadido el jar de JDBC a nuestras dependencies.

--- src/main/java/com/mycompany/artbalear/DataAcces/DataAcces.java
+++ src/main/java/com/mycompany/artbalear/DataAcces/DataAcces.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.artbalear.DataAcces;
 
-/**
- *
- * @author Alumne
- */
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
 public class DataAcces {
-    
+    private Connection getConnection() {
+        Connection connection = null;
+        Properties properties = new Properties();
+        try {
+            properties.load(DataAcces.class.getClassLoader().getResourceAsStream("application.properties"));
+            connection = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return connection;
+    }
 }
